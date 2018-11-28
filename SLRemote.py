55639003abc5e527993309f8094b47556056ab88
haldean <me@haldean.org>
2018-11-28 14:08:53 -0800

    make the arlo the index, fix links for images

--- SLRemote.py
+++ SLRemote.py
@@ -4,62 +4,11 @@ from utils_light import *
 
 app = Flask(__name__)
 app.config.update(dict(
-    SECRET_KEY="powerful secretkey",
-    WTF_CSRF_SECRET_KEY="a csrf secret key"
+    SECRET_KEY="haldean rules lolol come at me",
+    WTF_CSRF_SECRET_KEY="a csrf secret key that also asserts haldean's excellence and handsomeness and other good qualities"
 ))
 
-
-
-
-@app.route('/index', methods=['GET', 'POST'])
-def index():
-    return render_template('index.html')
-
 @app.route('/', methods=['GET', 'POST'])
-def remote():
-    form = RemoteForm()
-    print('inside quick add routes')
-    if form.validate_on_submit(): 
-        print('form test msg:')
-
-        print(form.brightness.data)
-        change_pattern(form.pattern.data)
-        change_brightness(float(form.brightness.data))
-        change_color(float(form.color.data))
-        change_speed(float(form.speed.data))
-        turn_on(form.on_off.data)
-     
-        if form.clip1.data:
-            print('clip1')
-            return redirect(url_for('remote'))
-        if form.clip2.data:
-            print('clip2')
-            return redirect(url_for('remote'))
-        if form.clip3.data:
-            print('clip3')
-            return redirect(url_for('remote'))
-        if form.submit.data:
-            print(('submit'))
-            return redirect(url_for('remote'))
-
-    return render_template('remote.html', form=form)
-
-@app.route('/test', methods=['GET', 'POST'])
-def test():
-    form = TestForm(csrf_enabled=False)
-    print(form.age.data)
-
-    if form.validate():
-        print(request.form["name_of_slider"])
-        print('form test msg:')
-        print(form.age.data)
-        
-        #print(request.form["outputtest"])
-        return redirect(url_for('main.test'))
-
-    return render_template('test.html', form=form)
-
-@app.route('/arlo', methods=['GET', 'POST'])
 def arlo():
     form = ArloForm()
     if form.validate_on_submit(): 
